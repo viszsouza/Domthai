ce52379c039afecfaa661d0d3db935fdfe995964
vizssouza <vini.nelso@gmail.com>
2025-07-23 21:14:12 -0300

    Adicionando medias querys
    Faltando apenas as funcionalidades e animações

--- js/scripts.js
+++ js/scripts.js
@@ -84,3 +84,11 @@ dots.forEach((dot, i) => {
         showSlide(imageIndex);
     });
 });
+
+// HAMBURGUER
+const hamburguer = document.querySelector(".hamburguer");
+const nav = document.querySelector(".nav");
+const navlist = document.querySelector(".nav-list")
+
+navlist.addEventListener("click", () => nav.classList.toggle("active"));
+hamburguer.addEventListener("click", () => nav.classList.toggle("active"));
